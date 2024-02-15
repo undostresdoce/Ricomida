6c99fb536e89d974b84a143957c9169653d0a44d
camilabelen <camictejo@gmail.com>
2024-02-14 22:49:46 -0300

    Proyecto Ricomida

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
+
+  $("#enviarCorreo").click(function () {
+    alert("El correo fue enviado correctamente...");
+  });
+
+  $(".titulo-rojo").on("dblclick", function () {
+    $(".titulo-rojo").css("color", "red");
+  });
+
+  $("#titulo-1").click(function () {
+    $("#img-1").toggle();
+  });
+
+  $("#titulo-2").click(function () {
+    $("#img-2").toggle();
+  });
+
+  $("#titulo-3").click(function () {
+    $("#img-3").toggle();
+  });
+});
